133a31830b5729d1ddafc8c7994f7ad30089f9e7
sadat103 <1505103.sbf@ugrad.cse.buet.ac.bd>
2020-12-08 00:06:38 +0600

    Run test done

--- Runstest.py
+++ Runstest.py
@@ -31,7 +31,10 @@ A=np.array([[ 4529.4,9044.9,  13568,  18091, 22615,27892],
        [27892,55789,83685,111580,139476,172860]])
 print("A matrix")
 print(A)
-
+array = np.arange(6)
+B = np.array([1/6,5/24,11/120,19/720,29/5040,1/840])
+print("B matrix")
+print(B)
 cnt = 1
 runs = []
 for i in range(1, len(uniform_numbers)):
@@ -45,12 +48,8 @@ for i in range(1, len(uniform_numbers)):
 # else cnt will be 1
 runs.append(cnt)
 r = np.array(runs)
+print("Run array")
 print(r)
-sum=0
-for i in range(0,len(r)):
-    sum = sum + r[i]
-print(sum)
-
 run_length_array = np.zeros(6)
 for i in range(0,len(r)):
     if r[i] ==1:
@@ -65,4 +64,20 @@ for i in range(0,len(r)):
         run_length_array[4]=run_length_array[4] +1
     else:
         run_length_array[5]=run_length_array[5] +1
+print("Run length array")
 print(run_length_array)
+
+sum = 0 
+for i in range(0,len(run_length_array)):
+    for j in range(0,len(run_length_array)):
+        p = A[i][j] * (run_length_array[i] - 500*B[i]) * (run_length_array[j]-500*B[j])
+        sum = sum + p
+R=sum/500
+print(R)
+a = stats.chi2.ppf(q=0.9,df=6)
+print(a)
+if(R > a):
+    print("Reject")
+else:
+    print("Accept")
+
