47b8207ac5a898e9322f26bb41ed2b832270d049
sadat103 <1505103.sbf@ugrad.cse.buet.ac.bd>
2020-12-09 03:43:45 +0600

    d=3 split done

--- Serialtest.py
+++ Serialtest.py
@@ -24,68 +24,78 @@ np.savetxt("Z.txt", Z, fmt="%s")
 np.savetxt("uniform.txt", uniform_numbers, fmt="%s")
 
 k =0
-N1 = np.zeros((int(N/d),d))
 
-for i in np.arange(0,N,d):
-   k = k+1
-   e = uniform_numbers[i:i+d]
-   for j in range(0,len(e)):
-       N1[k-1][j]= e[j]
+if d==2:
+    N1 = np.zeros((int(N/d),d))
 
-print(N1) 
+    for i in np.arange(0,N,d):
+        k = k+1
+        e = uniform_numbers[i:i+d]
+        for j in range(0,len(e)):
+             N1[k-1][j]= e[j]
 
-K_array = []
-a1 = math.sqrt(K**d)
-print(a1)
-inter_val =1/K
-a2 = int(a1)
-N2 = np.zeros((a2,a2))
-for i in range(0,len(N1)):
-    for j in range(0,d):
-        p = N1[i][j]
-        k=0
-        for m in np.arange(0,1,inter_val):
-            k = k+1
-            if np.logical_and(p >m , p <= m+inter_val):
-                print("%dth tuple  %dth number %f is between ( %f to %f ) and interval is %d "%(i+1,j+1,p,m,m+inter_val,k))
-                K_array.append(k)
+    print(N1)
+
+    K_array = []
+    a1 = math.sqrt(K**d)
+    print(a1)
+    inter_val =1/K
+    a2 = int(a1)
+    N2 = np.zeros((a2,a2))
+    for i in range(0,len(N1)):
+        for j in range(0,d):
+            p = N1[i][j]
+            k=0
+            for m in np.arange(0,1,inter_val):
+                k = k+1
+                if np.logical_and(p >m , p <= m+inter_val):
+                    print("%dth tuple  %dth number %f is between ( %f to %f ) and interval is %d "%(i+1,j+1,p,m,m+inter_val,k))
+                    K_array.append(k)
                
-K_A = np.resize(np.array(K_array),(int(N/d),d))
-np.savetxt("Serial_K_array.txt", K_A, fmt="%s")
-for i in range(0,len(K_A)):
-    for j in range(0,d):
-        if j==0:
-            p = K_A[i][j]
-        elif j==1:
-            q = K_A[i][j]
-    for d in range(0,len(N2)):
-        for e in range(0,len(N2)):
-            if (d+1)==p and (e+1)==q:
-                N2[d][e] = N2[d][e] + 1
+    K_A = np.resize(np.array(K_array),(int(N/d),d))
+    np.savetxt("Serial_K_array.txt", K_A, fmt="%s")
+    for i in range(0,len(K_A)):
+        for j in range(0,d):
+            if j==0:
+                p = K_A[i][j]
+            elif j==1:
+                q = K_A[i][j]
+        for d in range(0,len(N2)):
+            for e in range(0,len(N2)):
+                if (d+1)==p and (e+1)==q:
+                    N2[d][e] = N2[d][e] + 1
 
-print("Interval Count Array")
-print(N2)
-'''
-ll=0
-for i in range(0,len(N2)):
-    for j in range(0,len(N2)):
-        ll = ll + N2[i][j]
-print(ll)
-'''
-sum=0
-for i in range (0,len(N2)):
-    for j in range(0,len(N2)):
-        t = (N2[i][j] - N/(K**d))**2
-        sum = sum + t
+    print("Interval Count Array")
+    print(N2)
+    '''
+    ll=0
+    for i in range(0,len(N2)):
+        for j in range(0,len(N2)):
+            ll = ll + N2[i][j]
+    print(ll)
+    '''
+    sum=0
+    for i in range (0,len(N2)):
+        for j in range(0,len(N2)):
+            t = (N2[i][j] - N/(K**d))**2
+            sum = sum + t
 
-CHi = ((K**d)/N)*sum
-print("Chi square is %f" %(CHi))
-a = stats.chi2.ppf(q=1-alpha,df=K**d -1)
-print("Chi ppf value")
-print(a)
-if(CHi > a):
-    print("Reject")
-else:
-    print("Accept")
+    CHi = ((K**d)/N)*sum
+    print("Chi square is %f" %(CHi))
+    a = stats.chi2.ppf(q=1-alpha,df=K**d -1)
+    print("Chi ppf value is %f" %(a))
+    if(CHi > a):
+        print("Reject")
+    else:
+        print("Accept")
 
-        
+
+if d==3:
+    N1 = np.zeros((math.floor(N/d),d))
+
+    for i in np.arange(0,N,d):
+        k = k+1
+        e = uniform_numbers[i:i+d]
+        for j in range(0,len(e)):
+             N1[k-2][j]= e[j]
+    print(N1)
