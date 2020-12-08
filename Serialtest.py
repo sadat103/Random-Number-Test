1b7bf12865de48da9c9d08af66bb169b6eedc3de
sadat103 <1505103.sbf@ugrad.cse.buet.ac.bd>
2020-12-09 02:59:04 +0600

    serial d =2 done

--- Serialtest.py
+++ Serialtest.py
@@ -18,29 +18,26 @@ U[0] = 1505103/(2 ** 31)
 for i in range(1,N):
     Z[i] = (65539 * Z[i-1]) % (2 ** 31)
     U[i] = Z[i]/(2 ** 31)
-#print(Z)
 uniform_numbers = np.round_(U,6)
 print("Sub interval")
 np.savetxt("Z.txt", Z, fmt="%s")
 np.savetxt("uniform.txt", uniform_numbers, fmt="%s")
-#print(K)
-print("item per interval")
-#d= N/K
 
 k =0
-#print(uniform_numbers[1:4])
 N1 = np.zeros((int(N/d),d))
-print(N1)
+
 for i in np.arange(0,N,d):
    k = k+1
    e = uniform_numbers[i:i+d]
    for j in range(0,len(e)):
        N1[k-1][j]= e[j]
-inter_val =1/K
+
 print(N1) 
+
 K_array = []
 a1 = math.sqrt(K**d)
 print(a1)
+inter_val =1/K
 a2 = int(a1)
 N2 = np.zeros((a2,a2))
 for i in range(0,len(N1)):
@@ -65,16 +62,25 @@ for i in range(0,len(K_A)):
         for e in range(0,len(N2)):
             if (d+1)==p and (e+1)==q:
                 N2[d][e] = N2[d][e] + 1
+
+print("Interval Count Array")
 print(N2)
+ll=0
+for i in range(0,len(N2)):
+    for j in range(0,len(N2)):
+        ll = ll + N2[i][j]
+print(ll)
+
 sum=0
 for i in range (0,len(N2)):
     for j in range(0,len(N2)):
         t = (N2[i][j] - N/(K**d))**2
         sum = sum + t
-        #print(sum)
+
 CHi = ((K**d)/N)*sum
 print("Chi square is %f" %(CHi))
 a = stats.chi2.ppf(q=1-alpha,df=K**d -1)
+print("Chi ppf value")
 print(a)
 if(CHi > a):
     print("Reject")
