c431931967da24161ed5e29f22821a38aab28a3d
sadat103 <1505103.sbf@ugrad.cse.buet.ac.bd>
2020-12-09 02:47:13 +0600

    for d =2 test done

--- Serialtest.py
+++ Serialtest.py
@@ -7,15 +7,15 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from scipy import stats
 #def random_number(n):
-#N = int(input ("Enter value of n :")) 
-#K = int(input  ("Enter value of k :"))
-#alpha = float(input  ("Enter value of alpha :"))
-U = np.zeros(500)
-Z = np.zeros(500)
+N = int(input ("Enter value of n :")) 
+K = int(input  ("Enter value of k :"))
+d = int(input  ("Enter value of d :"))
+alpha = float(input  ("Enter value of alpha :"))
+U = np.zeros(N)
+Z = np.zeros(N)
 Z[0] = 1505103
 U[0] = 1505103/(2 ** 31)
-    #print(U)
-for i in range(1,500):
+for i in range(1,N):
     Z[i] = (65539 * Z[i-1]) % (2 ** 31)
     U[i] = Z[i]/(2 ** 31)
 #print(Z)
@@ -29,39 +29,37 @@ print("item per interval")
 
 k =0
 #print(uniform_numbers[1:4])
-N1 = np.zeros((250,2))
+N1 = np.zeros((int(N/d),d))
 print(N1)
-for i in np.arange(0,500,2):
+for i in np.arange(0,N,d):
    k = k+1
-   e = uniform_numbers[i:i+2]
-   print("Total number in %dth tuple is %d "%(k,len(e)))
-   print(uniform_numbers[i:i+2])
+   e = uniform_numbers[i:i+d]
    for j in range(0,len(e)):
        N1[k-1][j]= e[j]
-   print("-------------------------------------------------------------------------------")
-
-print(N1)
-print(N1[0][0]+1)
+inter_val =1/K
+print(N1) 
 K_array = []
-N2 = np.zeros((4,4))
+a1 = math.sqrt(K**d)
+print(a1)
+a2 = int(a1)
+N2 = np.zeros((a2,a2))
 for i in range(0,len(N1)):
-    for j in range(0,2):
+    for j in range(0,d):
         p = N1[i][j]
         k=0
-        for m in np.arange(0,1,0.25):
+        for m in np.arange(0,1,inter_val):
             k = k+1
-            if np.logical_and(p >m , p <= m+0.25):
-                print("%f  number in %f to %f interval and k and j is %d and %d "%(p,m,m+0.25,k,j))
+            if np.logical_and(p >m , p <= m+inter_val):
+                print("%dth tuple  %dth number %f is between ( %f to %f ) and interval is %d "%(i+1,j+1,p,m,m+inter_val,k))
                 K_array.append(k)
                
-K_A = np.resize(np.array(K_array),(250,2))
-#print(K_A)
+K_A = np.resize(np.array(K_array),(int(N/d),d))
 np.savetxt("Serial_K_array.txt", K_A, fmt="%s")
 for i in range(0,len(K_A)):
-    for j in range(0,2):
+    for j in range(0,d):
         if j==0:
             p = K_A[i][j]
-        else:
+        elif j==1:
             q = K_A[i][j]
     for d in range(0,len(N2)):
         for e in range(0,len(N2)):
@@ -71,12 +69,12 @@ print(N2)
 sum=0
 for i in range (0,len(N2)):
     for j in range(0,len(N2)):
-        t = (N2[i][j] - 500/(4**2))**2
+        t = (N2[i][j] - N/(K**d))**2
         sum = sum + t
         #print(sum)
-CHi = ((4**2)/500)*sum
+CHi = ((K**d)/N)*sum
 print("Chi square is %f" %(CHi))
-a = stats.chi2.ppf(q=0.9,df=15)
+a = stats.chi2.ppf(q=1-alpha,df=K**d -1)
 print(a)
 if(CHi > a):
     print("Reject")
